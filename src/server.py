88e53c5ec30fed4fa3c8839f4872bdb76a518bc4
stephan-carstens <carstensstephan@outlook.com>
2025-02-26 08:53:37 +0200

    add debug output for write

--- src/server.py
+++ src/server.py
@@ -226,7 +226,7 @@ class Server(ABC):
         values = self._encoded(value, dtype)
 
         logger.info(
-            f"Writing param {parameter_name} ({register_type}) of {dtype=} from {address=}, {multiplier=}, {count=}, {modbus_id=}")
+            f"Writing {values} to param {parameter_name} ({register_type}) of {dtype=} from {address=}, {multiplier=}, {count=}, {modbus_id=}")
 
         result = self.connected_client.write(values, address, modbus_id, register_type)
 
